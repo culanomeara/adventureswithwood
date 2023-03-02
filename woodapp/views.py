2e429bbe5fcaf9873b291c2b3736fbd89503c1ac
Culan O'Meara <culanomeara@gmail.com>
2023-03-02 20:36:31 +0000

    Create Projectlist view and html templates

--- woodapp/views.py
+++ woodapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, Project
 
-# Create your views here.
+
+class ProjectList(generic.ListView):
+    model = Project
+    queryset = Project.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 4
