7d5c41207347d62b730c75e2bfa6cacf9933a046
Culan O'Meara <culanomeara@gmail.com>
2023-03-05 20:10:59 +0000

    Add navs for project and post pages, more detail to details pages

--- woodapp/urls.py
+++ woodapp/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("", views.ProjectList.as_view(), name="home"),
     path('posts/', views.PostList.as_view(), name='posts'),
+    path('projects/', views.ProjectList.as_view(), name='projects'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_details'),
+    path('<slug:slug>/',
+         views.ProjectDetail.as_view(), name='project_details'),
 ]
--- woodapp/views.py
+++ woodapp/views.py
@@ -6,7 +6,7 @@ from .models import Post, Project
 class ProjectList(generic.ListView):
     model = Project
     queryset = Project.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'projects.html'
     paginate_by = 4
 
 
@@ -22,16 +22,38 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
+        # comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
             request,
-            "post_detail.html",
+            "post_details.html",
             {
                 "post": post,
+                # "comments": comments,
+                "liked": liked
+            },
+        )
+
+
+class ProjectDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Project.objects.filter(status=1)
+        project = get_object_or_404(queryset, slug=slug)
+        comments = Project.comments.filter(
+                approved=True).order_by("-created_on")
+        liked = False
+        if project.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "project_details.html",
+            {
+                "project": project,
                 "comments": comments,
                 "liked": liked
             },
