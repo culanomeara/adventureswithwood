a5a1201ef16f5f00bb4983f6fc46101c03fce5f2
Culan O'Meara <culanomeara@gmail.com>
2023-04-10 21:17:00 +0000

    Add tests for index, posts and projects pages

--- woodapp/tests.py
+++ woodapp/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHomeViews(TestCase):
+    """
+    A class for testing the home page views
+    """
+    def test_get_home_page(self):
+        """
+        This test checks if the home page is displayed properly
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+
+class TestPostsViews(TestCase):
+    """
+    A class for testing posts views
+    """
+    def test_get_posts_page(self):
+        """
+        This test checks if the posts page is displayed
+        """
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts.html')
+
+
+class TestProjectsViews(TestCase):
+    """
+    A class for testing projects views
+    """
+    def test_get_projects_page(self):
+        """
+        This test checks if the projects page is displayed
+        """
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'projects.html')
