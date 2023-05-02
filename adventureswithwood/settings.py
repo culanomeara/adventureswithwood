635aa7fd516821bba8487e2ce1c1fbc73891fa91
Culan O'Meara <culanomeara@gmail.com>
2023-05-02 20:19:39 +0000

    Fix deployment error

--- adventureswithwood/settings.py
+++ adventureswithwood/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG'
+DEBUG = False
 
 ALLOWED_HOSTS = ['adventureswithwood.herokuapp.com', 'localhost']
 X_FRAME_OPTIONS = 'SAMEORIGIN'
